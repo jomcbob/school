e3c441598dfea2ad3ac21bc19f03729bd5558576
andy <andrew@risertech.com>
2025-02-25 12:20:48 -0700

    fixed it adding people when there was no one in the inputs

--- script.js
+++ script.js
@@ -9,22 +9,25 @@ function randomizeSeats() {
     const kio = "keo matsura"
     const andrew = "andrew borlin"
 
-    const lowerCaseStudentNames = studentNames.map(name => name.toLowerCase());
-    const lowerCaseKio = kio.toLowerCase()
-    const lowerCaseAndrew = andrew.toLowerCase()
+    // Remove Andrew and Keo from the list if they exist
+    let indexKio = studentNames.indexOf(kio)
+    let indexAndrew = studentNames.indexOf(andrew)
 
-    let indexKio = lowerCaseStudentNames.indexOf(lowerCaseKio);
-    let indexAndrew = lowerCaseStudentNames.indexOf(lowerCaseAndrew);
+    if (indexKio > -1){
+     studentNames.splice(indexKio, 1)
+     indexAndrew = studentNames.indexOf(andrew)
+    }
+    if (indexAndrew > -1) studentNames.splice(indexAndrew, 1)
 
-    if (indexKio > -1) studentNames.splice(indexKio, 1);
-    if (indexAndrew > -1) studentNames.splice(indexAndrew, 1);
+    let pair = [];
+    if (indexKio > -1 && indexAndrew > -1) {
+        pair = [andrew, kio]
+    }
 
-    const pair = [andrew, kio]
-
-    const shuffledNames = [...studentNames];
+    const shuffledNames = [...studentNames]
     for (let i = shuffledNames.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledNames[i], shuffledNames[j]] = [shuffledNames[j], shuffledNames[i]];
+        const j = Math.floor(Math.random() * (i + 1))
+        [shuffledNames[i], shuffledNames[j]] = [shuffledNames[j], shuffledNames[i]]
     }
 
     const validHorizontalPairs = [
@@ -34,19 +37,20 @@ function randomizeSeats() {
         [19, 20], [21, 22], [23, 24],
         [25, 26], [27, 28], [29, 30],
         [31, 32], [33, 34], [35, 36]
-    ];
+    ]
 
-    const selectedPair = validHorizontalPairs[Math.floor(Math.random() * validHorizontalPairs.length)];
+    const selectedPair = validHorizontalPairs[Math.floor(Math.random() * validHorizontalPairs.length)]
 
-    let insertPosition1 = selectedPair[0] - 1
-    let insertPosition2 = selectedPair[1] - 1
+    let insertPosition1 = selectedPair[0] - 1;
+    let insertPosition2 = selectedPair[1] - 1;
 
-    shuffledNames.splice(insertPosition1, 0, pair[0]);
-    shuffledNames.splice(insertPosition2, 0, pair[1]);
+    if (pair.length > 0) {
+        shuffledNames.splice(insertPosition1, 0, pair[0])
+        shuffledNames.splice(insertPosition2, 0, pair[1])
+    }
 
     for (let i = 1; i <= 36; i++) {
         const desk = document.getElementById(`seat${i}`);
         desk.textContent = shuffledNames[i - 1];
     }
 }
-
